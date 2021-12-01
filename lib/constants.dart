f2b0ef411603e4b347d7eade43f99876d15b0b3d
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-01 21:22:04 +0300

    added new colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,14 +3,14 @@ import 'package:flutter/material.dart';
 
 const TextStyle myStyle = TextStyle(
   fontSize: 35,
-  fontFamily: 'SourceSansPro',
+  fontFamily: 'ScheherazadeNew',
   color: textColor,
 );
 
 
-const Color cardColor = Color(0xFF6B2D5C);
-const Color backgroundColor = Color(0xFF011C27);
-const Color accentColor = Color(0xFF1B4B5A);
-const Color textColor = Color(0xFFFFFFFF);
+const Color cardColor = Color(0xFF0A1045);
+const Color backgroundColor = Color(0xFF00C2D1);
+const Color accentColor = Color(0xFFF9E900);
+const Color textColor = Color(0xFFEAFFDA);
 const Color buttonColor = Color(0xFF545677);
-const Color floatactionButtonColor = Color(0xFF20BF55);
+const Color floatactionButtonColor = Color(0xFF0A1045);
