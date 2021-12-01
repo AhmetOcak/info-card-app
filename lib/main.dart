fd6574d1a569fc079a01f759281da335e0392f76
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-01 22:09:54 +0300

    added provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:info_card_app/models/cards_data.dart';
 import 'package:info_card_app/screens/create_category.dart';
 import 'package:info_card_app/screens/create_info_card.dart';
 import 'package:info_card_app/screens/info_cards.dart';
 import 'package:info_card_app/screens/my_info_card.dart';
+import 'package:provider/provider.dart';
 import 'screens/edit_my_info_card.dart';
 import 'screens/home.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    MaterialApp(
-      initialRoute: '/home',
-      routes: {
-        '/home': (context) => const MyApp(),
-        '/createCategory': (context) => const CreateCategory(),
-        '/infoCards': (context) => const  InfoCardScreen(catId: null,),
-        '/createInfoCards': (context) => const CreateInfoCard(catId: null,),
-        '/editInfoCard': (context) => EditInfoCard(cardName: '',),
-        '/myCard': (context) => const MyCard(cardName: '', catId: null, id: null, data: '',),
-      },
-      debugShowCheckedModeBanner: false,
+    ChangeNotifierProvider(
+      create: (context) => CardsData(),
+      child: MaterialApp(
+        initialRoute: '/home',
+        routes: {
+          '/home': (context) => const MyApp(),
+          '/createCategory': (context) => const CreateCategory(),
+          '/infoCards': (context) => const  InfoCardScreen(catId: null,),
+          '/createInfoCards': (context) => const CreateInfoCard(catId: null,),
+          '/editInfoCard': (context) => EditInfoCard(cardName: '',),
+          '/myCard': (context) => const MyCard(cardName: '', catId: null, id: null, data: '',),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
     ),
   );
 }
