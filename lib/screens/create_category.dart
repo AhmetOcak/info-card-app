19e9b292854b043c46d053ed7e685787e20b56ae
AhmetOcak <73544434+AhmetOcak@users.noreply.github.com>
2021-12-21 23:32:02 +0300

    edit

--- lib/screens/create_category.dart
+++ lib/screens/create_category.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_key_in_widget_constructors, must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:info_card_app/constants/constants.dart';
 import 'package:info_card_app/models/cards_data.dart';
--- lib/screens/create_info_card.dart
+++ lib/screens/create_info_card.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 import 'package:info_card_app/constants/constants.dart';
 import 'package:info_card_app/models/cards_data.dart';
